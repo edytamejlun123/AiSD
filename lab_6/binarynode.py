b7655897bdb728f84aa157b1f91952e3d8e6f47d
edytamejlun_ <92027759+edytamejlun123@users.noreply.github.com>
2022-12-15 01:23:42 +0100

    binarynode

--- lab_6/binarynode.py
+++ lab_6/binarynode.py
@@ -1,15 +1,18 @@
+from __future__ import annotations
 from typing import Any, List
 
 
 class BinaryNode:
     value: Any
-    left_child: 'BinaryNode'
-    right_child: 'BinaryNode'
+    left_child: BinaryNode
+    right_child: BinaryNode
 
-    def __init__(self, value: Any) -> None:
+    def __init__(self, value: Any = None) -> None:
         self.value = value
+        self.left_child = None
+        self.right_child = None
 
-    def min(self) -> 'BinaryNode':
+    def min(self) -> BinaryNode:
         counter = self
 
         while counter.left_child is not None:
@@ -19,23 +22,36 @@ class BinaryNode:
 
 
 class BinarySearchTree:
-    root: 'BinaryNode'
+    root: BinaryNode
 
-    def __init__(self, root: 'BinaryNode') -> None:
+    def __init__(self, root: BinaryNode) -> None:
         self.root = root
 
-    def _insert(self, node: 'BinaryNode', value: Any) -> 'BinaryNode':
-        self.root = node
-        if value < self.root.value:
-            self.root.left_child = self._insert(self.root.left_child, value)
+    # def _insert(self, node: 'BinaryNode', value: Any) -> 'BinaryNode':
+    #     self.root = node
+    #     if value < self.root.value:
+    #         self.root.left_child = self._insert(self.root.left_child, value)
+    #
+    #     if value >= self.root.value:
+    #         self.root.right_child = self._insert(self.root.right_child, value)
+    #
+    #     return BinaryNode(value)
 
-        if value >= self.root.value:
-            self.root.right_child = self._insert(self.root.right_child, value)
+    def _insert(self, node: BinaryNode, value: Any) -> BinaryNode:
+        if value < node.value:
+            if node.left_child is None:
+                node.left_child = BinaryNode(value)
+            self._insert(node.left_child, value)
+
+        if value >= node.value:
+            self._insert(node.right_child, value)
 
         return BinaryNode(value)
 
     def insert(self, value: Any) -> None:
-        self._insert(self.root, value)
+        a: BinaryNode = BinaryNode(value)
+        self._insert(a, value)
+        self.root = a
 
     def insert_list(self, lista: List[Any]) -> None:
         for i in lista:
@@ -47,17 +63,24 @@ class BinarySearchTree:
             self.root = self.root.left_child
             self.contains(value)
 
-        if value > self.root.value:
+        elif value > self.root.value:
             self.root = self.root.right_child
             self.contains(value)
 
-        if value == self.root.value:
+        elif value == self.root.value:
             return True
 
-        return False
+        else:
+            return False
 
-    def _remove(self, node: 'BinaryNode', value: Any) -> 'BinaryNode':
-        if self.root.left_child is None and self.root.right_child is None:
-            self.root = node
-            del node
-        if node.value > value:
+    # def _remove(self, node: 'BinaryNode', value: Any) -> 'BinaryNode':
+    #     if self.root.left_child is None and self.root.right_child is None:
+    #         self.root = node
+    #         del node
+    #     if self.root.value > value:
+
+    def __str__(self) -> str:
+        wynik = ''
+        if self.root:
+            if self.root.value:
+                wynik += (str(self.root.value) + '->')
--- lab_6/test-binary.py
+++ lab_6/test-binary.py
@@ -1,8 +1,25 @@
-from binarynode import BinaryNode
+from binarynode import *
 
 
 def main() -> None:
-    n1 = BinaryNode(4)
+    n1 = BinaryNode(8)
+    n2 = BinaryNode(3)
+    # n3 = BinaryNode(1)
+    # n4 = BinaryNode(10)
+
+    # n1.left_child = n2
+    # n1.right_child = n4
+    # n2.left_child = n3
+    #
+    # assert n1.min() == n3
+
+    r1 = BinarySearchTree(n1)
+    r2 = BinarySearchTree(n2)
+    r1.root.left_child = r2
+    # r1._insert(n1, 2)
+    # assert r1.root.min() ==
+    assert r1.contains(8) == True
+    assert r1.contains(3) == True
 
 
 if __name__ == "__main__":
